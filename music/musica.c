fa14183b5cf26730450288d7d44d2ce520393a23
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-05-19 18:34:42 -0300

    fix: loop da música

--- music/musica.c
+++ music/musica.c
@@ -1,27 +1,33 @@
 #include "musica.h"
 
+#define MUSICA_DURACAO_MAX 50.0f
+
 static Music musicaStarWars;
 
-void InitMusica(void) 
+void InitMusica(void)
 {
     InitAudioDevice();
     musicaStarWars = LoadMusicStream("music/starwars.wav");
-    musicaStarWars.looping = true;
+    musicaStarWars.looping = false;
     SetMusicVolume(musicaStarWars, 0.5f);
     PlayMusicStream(musicaStarWars);
 }
 
-void UpdateMusica(void) 
+void UpdateMusica(void)
 {
     UpdateMusicStream(musicaStarWars);
+    if (GetMusicTimePlayed(musicaStarWars) >= MUSICA_DURACAO_MAX) 
+    {
+        PlayMusicStream(musicaStarWars);
+    }
 }
 
-void StopMusica(void) 
+void StopMusica(void)
 {
     StopMusicStream(musicaStarWars);
 }
 
-void UnloadMusica(void) 
+void UnloadMusica(void)
 {
     UnloadMusicStream(musicaStarWars);
     CloseAudioDevice();
